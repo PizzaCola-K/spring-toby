4ac527a76037f453780ac6f78d1f82a418e7520c
K <PizzaCola.K@gmail.com>
2021-03-31 14:26:32 +0900

    test: Use BeforeEach

--- src/test/java/springboot/user/dao/UserDaoTest.java
+++ src/test/java/springboot/user/dao/UserDaoTest.java
@@ -1,9 +1,9 @@
 package springboot.user.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import springboot.user.domain.User;
 
@@ -14,61 +14,60 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class UserDaoTest {
 
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+
+    @BeforeEach
+    public void setUp() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        userDao = context.getBean("userDao", UserDao.class);
+        user1 = new User("gyumee", "박성철", "springno1");
+        user2 = new User("leegw700", "이길원", "springno2");
+        user3 = new User("bumjin", "박범진", "springno3");
+    }
+
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        User user1 = new User("gyumee", "박성철", "springno1");
-        User user2 = new User("leegw700", "이길원", "springno2");
-
-        dao.add(user1);
-        dao.add(user2);
-        assertThat(dao.getCount()).isEqualTo(2);
-
-        User userGet1 = dao.get(user1.getId());
+        User userGet1 = userDao.get(user1.getId());
         assertThat(userGet1.getName()).isEqualTo(user1.getName());
         assertThat(userGet1.getPassword()).isEqualTo(user1.getPassword());
 
-        User userGet2 = dao.get(user2.getId());
+        User userGet2 = userDao.get(user2.getId());
         assertThat(userGet2.getName()).isEqualTo(user2.getName());
         assertThat(userGet2.getPassword()).isEqualTo(user2.getPassword());
     }
 
     @Test
     public void count() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
-        User user1 = new User("gyumee", "박성철", "springno1");
-        User user2 = new User("leegw700", "이길원", "springno2");
-        User user3 = new User("bumjin", "박범진", "springno3");
+        userDao.add(user1);
+        assertThat(userDao.getCount()).isEqualTo(1);
 
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        dao.add(user1);
-        assertThat(dao.getCount()).isEqualTo(1);
-
-        dao.add(user2);
-        assertThat(dao.getCount()).isEqualTo(2);
-
-        dao.add(user3);
-        assertThat(dao.getCount()).isEqualTo(3);
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
     }
 
     @Test
     public void getUserFailure() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
         assertThatExceptionOfType(EmptyResultDataAccessException.class)
-                .isThrownBy(() -> dao.get("unknown_id"));
+                .isThrownBy(() -> userDao.get("unknown_id"));
     }
 
 }
