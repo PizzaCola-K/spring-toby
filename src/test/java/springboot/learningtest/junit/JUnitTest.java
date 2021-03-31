e0dfa18cc3785c738e0a4915d4926705602bd9d7
K <PizzaCola.K@gmail.com>
2021-03-31 15:26:39 +0900

    test: applicationContext

--- src/test/java/springboot/learningtest/junit/JUnitTest.java
+++ src/test/java/springboot/learningtest/junit/JUnitTest.java
@@ -1,31 +1,46 @@
 package springboot.learningtest.junit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/junit.xml")
 class JUnitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
-        assertThat(testObjects).doesNotContain(this);
-        testObjects.add(this);
+        test();
     }
 
 
     @Test
     public void test2() {
-        assertThat(testObjects).doesNotContain(this);
-        testObjects.add(this);
+        test();
     }
 
     @Test
     public void test3() {
+        test();
+    }
+
+    private void test() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 }
