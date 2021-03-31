07ce6ec1da00dd7018a89713b26d0d7cf09e4595
K <PizzaCola.K@gmail.com>
2021-03-31 11:32:55 +0900

    test: Add UserDaoTest

---
+++ src/test/java/springboot/user/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package springboot.user.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springboot.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+
+}
