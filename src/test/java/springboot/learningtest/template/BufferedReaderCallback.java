d4d354d67f4462620a5b44500999b839561d8b2e
K <PizzaCola.K@gmail.com>
2021-04-01 18:37:54 +0900

    test: lineReadTemplate

--- src/test/java/springboot/learningtest/template/BufferedReaderCallback.java
+++ src/test/java/springboot/learningtest/template/BufferedReaderCallback.java
@@ -1,8 +1,7 @@
 package springboot.learningtest.template;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
 public interface BufferedReaderCallback {
-    Integer doSomethingWithReader(BufferedReader br) throws IOException;
+    Integer doSomethingWithLine(String line, Integer value) throws IOException;
 }
--- src/test/java/springboot/learningtest/template/Calculator.java
+++ src/test/java/springboot/learningtest/template/Calculator.java
@@ -6,30 +6,20 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filePath) throws IOException {
-        return fileReadTemplate(filePath, br -> {
-            Integer sum = 0;
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-            return sum;
-        });
+        return lineReadTemplate(filePath, 0, (line, value) -> value + Integer.parseInt(line));
     }
 
     public Integer calcMultiply(String filePath) throws IOException {
-        return fileReadTemplate(filePath, br -> {
-            Integer mulipliy = 1;
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                mulipliy *= Integer.valueOf(line);
-            }
-            return mulipliy;
-        });
+        return lineReadTemplate(filePath, 1, (line, value) -> value * Integer.parseInt(line));
     }
 
-    public Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+    public Integer lineReadTemplate(String filePath, int initVal, BufferedReaderCallback callback) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            int result = callback.doSomethingWithReader(br);
+            Integer result = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                result = callback.doSomethingWithLine(line, result);
+            }
             return result;
         }
     }
