4e1a635d94db18f609aef77b8b729795666f55cc
K <PizzaCola.K@gmail.com>
2021-06-24 21:48:23 +0900

    feat: Send email

--- src/main/java/springboot/user/service/UserService.java
+++ src/main/java/springboot/user/service/UserService.java
@@ -1,18 +1,22 @@
 package springboot.user.service;
 
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import springboot.user.dao.UserDao;
 import springboot.user.domain.Level;
 import springboot.user.domain.User;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class UserService {
 
@@ -54,6 +58,28 @@ public class UserService {
     protected void upgradeLevel(User user) {
         userLevelUpgradePolicy.upgradeLevel(user);
         userDao.update(user);
+        sendUpgradeEMail(user);
+    }
+
+    private void sendUpgradeEMail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다.");
+
+            Transport.send(message);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void add(User user) {
